9e9324ba3d228144381baafa1adf99aef51811b0
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-01 10:46:55 +0200

    Classes and Interface files added.

---
+++ TeamWebshopProject.API/CSRD/Repository/Interfaces/ITagRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamWebshopProject.API.CSRD.Repository.Interfaces
+{
+    interface ITagRepository
+    {
+    }
+}
