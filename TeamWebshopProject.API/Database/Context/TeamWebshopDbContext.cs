36abb314776daab6ff30df494f9f8f5240453c55
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-02 08:13:45 +0200

    Context added from merge with Migrations branch Work on repositories will start now

--- TeamWebshopProject.API/Database/Context/TeamWebshopDbContext.cs
+++ TeamWebshopProject.API/Database/Context/TeamWebshopDbContext.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TeamWebshopProject.API.Models;
 
-namespace TeamWebshopProject.API.Database
+namespace TeamWebshopProject.API.Database.Context
 {
     public class TeamWebshopDbContext : DbContext
     {
