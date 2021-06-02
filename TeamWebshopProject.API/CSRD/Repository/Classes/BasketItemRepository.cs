2509e895d910b0ebcfea4c0db7d399619c6072a8
Eric Bisbjerg <eric.bisbjerg@hotmail.com>
2021-06-02 12:52:16 +0200

    BasketItemRepo and interface updated to standard CRUD

--- TeamWebshopProject.API/CSRD/Repository/Classes/BasketItemRepository.cs
+++ TeamWebshopProject.API/CSRD/Repository/Classes/BasketItemRepository.cs
@@ -4,23 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using TeamWebshopProject.API.CSRD.Repository.Interfaces;
+using TeamWebshopProject.API.Database.Context;
 using TeamWebshopProject.API.Models;
 
 namespace TeamWebshopProject.API.CSRD.Repository.Classes
 {
     public class BasketItemRepository : IBasketItemRepository
     {
-        public Task<BasketItem> GetBasketItem(int id)
+        private readonly TeamWebshopDbContext _context;
+        public BasketItemRepository(TeamWebshopDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<BasketItem> Create(BasketItem basketItem)
         {
             throw new NotImplementedException();
         }
 
-        public Task<BasketItem> GetBasketItemQuantity()
+        public Task<BasketItem> Delete(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<BasketItem>> GetBasketItems()
+        public Task<BasketItem> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BasketItem>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketItem> Update(int id, BasketItem basketItem)
         {
             throw new NotImplementedException();
         }
