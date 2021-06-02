f1e808cdf53b0ec01c6eeb654faaf581ad752f22
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-02 08:56:01 +0200

    Dont mind this

--- TeamWebshopProject.API/CSRD/Repository/Classes/BasketItemRepository.cs
+++ TeamWebshopProject.API/CSRD/Repository/Classes/BasketItemRepository.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using TeamWebshopProject.API.CSRD.Repository.Interfaces;
+using TeamWebshopProject.API.Models;
+
 namespace TeamWebshopProject.API.CSRD.Repository.Classes
 {
-    public class BasketItemRepository
+    public class BasketItemRepository : IBasketItemRepository
     {
+        public Task<BasketItem> GetBasketItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketItem> GetBasketItemQuantity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BasketItem>> GetBasketItems()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
