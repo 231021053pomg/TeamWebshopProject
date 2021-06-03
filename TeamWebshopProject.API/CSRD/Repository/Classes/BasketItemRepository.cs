7c2a3ee9e917d352899a98d3a3e7d42cc12e31fb
Eric Bisbjerg <eric.bisbjerg@hotmail.com>
2021-06-03 12:55:29 +0200

    Ignore commit

--- TeamWebshopProject.API/CSRD/Repository/Classes/BasketItemRepository.cs
+++ TeamWebshopProject.API/CSRD/Repository/Classes/BasketItemRepository.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using TeamWebshopProject.API.CSRD.Repository.Interfaces;
+using TeamWebshopProject.API.Database.Context;
 using TeamWebshopProject.API.Models;
 
 namespace TeamWebshopProject.API.CSRD.Repository.Classes
 {
     public class BasketItemRepository : IBasketItemRepository
     {
+        private readonly TeamWebshopDbContext _context;
+        public BasketItemRepository(TeamWebshopDbContext context)
+        {
+            _context = context;
+        }
         public Task<BasketItem> GetBasketItem(int id)
         {
             throw new NotImplementedException();
