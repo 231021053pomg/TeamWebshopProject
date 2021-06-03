3cc8c4cec7e8e7b3b12847322ef9708fe3351366
Eric Bisbjerg <eric.bisbjerg@hotmail.com>
2021-06-03 08:15:39 +0200

    Added all test files with regions

---
+++ TeamWebshopProject.Test/CSRD/Repository/Tag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWebshopProject.Test.CSRD.Repository
+{
+    public class Tag
+    {
+        #region setup
+        #endregion
+
+        #region GetAll
+        #endregion
+
+        #region Get
+        #endregion
+
+        #region Create
+        #endregion
+
+        #region Update
+        #endregion
+
+        #region Delete
+        #endregion
+    }
+}
