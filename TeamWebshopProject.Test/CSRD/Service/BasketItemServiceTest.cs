7602afe2aa2969b532131a12255343a0bb167923
Eric Bisbjerg <eric.bisbjerg@hotmail.com>
2021-06-07 09:07:30 +0200

    BasketItem test done

--- TeamWebshopProject.Test/CSRD/Service/BasketItemServiceTest.cs
+++ TeamWebshopProject.Test/CSRD/Service/BasketItemServiceTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using TeamWebshopProject.API.CSRD.Repository.Classes;
+using TeamWebshopProject.API.CSRD.Services.Classes;
+using TeamWebshopProject.API.Models;
 using Xunit;
 
 namespace TeamWebshopProject.Test.CSRD.Service
@@ -11,22 +14,113 @@ namespace TeamWebshopProject.Test.CSRD.Service
    public class BasketItemServiceTest
     {
         #region Setup
+        private readonly BasketItemService _basketItemService;
+        private readonly Mock<BasketItemRepository> _mock = new();
+
+        public BasketItemServiceTest()
+        {
+            _basketItemService = new(_mock.Object);
+        }
         #endregion
 
         #region GetAll
+        [Fact]
+        public async Task GetAll_Success_Test()
+        {
+            // Arrange
+            int expectedCount = 2;
+            List<BasketItem> basketItems = new List<BasketItem> { new BasketItem { Id = 1 }, new BasketItem { Id = 2 } };
+            _mock
+                .Setup(x => x.GetAll())
+                .ReturnsAsync(basketItems);
+
+            // Act
+            var result = await _basketItemService.GetAll();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedCount, result.Count());
+        }
         #endregion
 
         #region Get
+        [Fact]
+        public async Task Get_Success_Test()
+        {
+            // Arrange
+            BasketItem basketItem = new BasketItem { Id = 1, Item = new Item { Id = 1 } };
+            
+            _mock
+                .Setup(x => x.Get(It.IsAny<int>()))
+                .ReturnsAsync(basketItem);
+
+            // Act
+            var result = await _basketItemService.Get(It.IsAny<int>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(basketItem.Id, result.Id);
+        }
         #endregion
 
         #region Create
+        [Fact]
+        public async Task Create_Success_Test()
+        {
+            // Arrange
+            BasketItem newBasketItem = new BasketItem { Id = 1, Item = new Item { Id = 1 } };
+
+            _mock
+                .Setup(x => x.Create(It.IsAny<BasketItem>()))
+                .ReturnsAsync(newBasketItem);
+
+            // Act
+            var result = await _basketItemService.Create(It.IsAny<BasketItem>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(newBasketItem.Id, result.Id);
+        }
         #endregion
 
         #region Update
+        [Fact]
+        public async Task Update_Success_Test()
+        {
+            // Arrange
+            BasketItem basketItem = new BasketItem { Id = 1, Item = new Item { Id = 1 } };
+
+            _mock
+                .Setup(x => x.Create(It.IsAny<BasketItem>()))
+                .ReturnsAsync(basketItem);
+
+            // Act
+            var result = await _basketItemService.Update(It.IsAny<int>(), It.IsAny<BasketItem>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(basketItem.Id, result.Id);
+        }
         #endregion
 
         #region Delete
-        #endregion
+        [Fact]
+        public async Task Delete_Success_Test()
+        {
+            // Arrange
+            BasketItem basketItem = new BasketItem { Id = 1, Item = new Item { Id = 1 } };
 
+            _mock
+                .Setup(x => x.Delete(It.IsAny<int>()))
+                .ReturnsAsync(basketItem);
+
+            // Act
+            var result = await _basketItemService.Delete(It.IsAny<int>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(basketItem.Id, result.Id);
+        }
+        #endregion
     }
 }
