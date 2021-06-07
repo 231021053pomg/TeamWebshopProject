0b8abdcac3a70626a8f7b3efcf142b32419d9678
Eric Bisbjerg <eric.bisbjerg@hotmail.com>
2021-06-07 10:09:11 +0200

    Order OrderItem Tag service test a-ok

--- TeamWebshopProject.Test/CSRD/Service/OrderItemServiceTest.cs
+++ TeamWebshopProject.Test/CSRD/Service/OrderItemServiceTest.cs
@@ -1,12 +1,126 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamWebshopProject.API.CSRD.Repository.Classes;
+using TeamWebshopProject.API.CSRD.Services.Classes;
+using TeamWebshopProject.API.Models;
+using Xunit;
 
 namespace TeamWebshopProject.Test.CSRD.Service
 {
-    class OrderItemServiceTest
+    public class OrderItemServiceTest
     {
+        #region Setup
+        private readonly OrderItemService _customerService;
+        private readonly Mock<OrderItemRepository> _mock = new();
+
+        public OrderItemServiceTest()
+        {
+            _customerService = new(_mock.Object);
+        }
+        #endregion
+
+        #region GetAll
+        [Fact]
+        public async Task GetAll_Success_Test()
+        {
+            // Arrange
+            int expectedCount = 2;
+            List<OrderItem> orderItems = new List<OrderItem> { new OrderItem { Id = 1 }, new OrderItem { Id = 2 } };
+            _mock
+                .Setup(x => x.GetAll())
+                .ReturnsAsync(orderItems);
+
+            // Act
+            var result = await _customerService.GetAll();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedCount, result.Count());
+        }
+        #endregion
+
+        #region Get
+        [Fact]
+        public async Task Get_Success_Test()
+        {
+            // Arrange
+            OrderItem orderItem = new OrderItem { Id = 1 };
+
+            _mock
+                .Setup(x => x.Get(It.IsAny<int>()))
+                .ReturnsAsync(orderItem);
+
+            // Act
+            var result = await _customerService.Get(It.IsAny<int>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(orderItem.Id, result.Id);
+        }
+        #endregion
+
+        #region Create
+        [Fact]
+        public async Task Create_Success_Test()
+        {
+            // Arrange
+            OrderItem orderItem = new OrderItem { Id = 1 };
+
+            _mock
+                .Setup(x => x.Create(It.IsAny<OrderItem>()))
+                .ReturnsAsync(orderItem);
+
+            // Act
+            var result = await _customerService.Create(It.IsAny<OrderItem>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(orderItem.Id, result.Id);
+        }
+        #endregion
+
+        #region Update
+        [Fact]
+        public async Task Update_Success_Test()
+        {
+            // Arrange
+            OrderItem orderItem = new OrderItem { Id = 1 };
+
+            _mock
+                .Setup(x => x.Create(It.IsAny<OrderItem>()))
+                .ReturnsAsync(orderItem);
+
+            // Act
+            var result = await _customerService.Update(It.IsAny<int>(), It.IsAny<OrderItem>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(orderItem.Id, result.Id);
+        }
+        #endregion
+
+        #region Delete
+        [Fact]
+        public async Task Delete_Success_Test()
+        {
+            // Arrange
+            OrderItem orderItem = new OrderItem { Id = 1 };
+
+            _mock
+                .Setup(x => x.Delete(It.IsAny<int>()))
+                .ReturnsAsync(orderItem);
+
+            // Act
+            var result = await _customerService.Delete(It.IsAny<int>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(orderItem.Id, result.Id);
+        }
+        #endregion
     }
 }
--- TeamWebshopProject.Test/CSRD/Service/OrderServiceTest.cs
+++ TeamWebshopProject.Test/CSRD/Service/OrderServiceTest.cs
@@ -1,12 +1,126 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamWebshopProject.API.CSRD.Repository.Classes;
+using TeamWebshopProject.API.CSRD.Services.Classes;
+using TeamWebshopProject.API.Models;
+using Xunit;
 
 namespace TeamWebshopProject.Test.CSRD.Service
 {
-    class OrderServiceTest
+    public class OrderServiceTest
     {
+        #region Setup
+        private readonly OrderService _customerService;
+        private readonly Mock<OrderRepository> _mock = new();
+
+        public OrderServiceTest()
+        {
+            _customerService = new(_mock.Object);
+        }
+        #endregion
+
+        #region GetAll
+        [Fact]
+        public async Task GetAll_Success_Test()
+        {
+            // Arrange
+            int expectedCount = 2;
+            List<Order> orders = new List<Order> { new Order { Id = 1 }, new Order { Id = 2 } };
+            _mock
+                .Setup(x => x.GetAll())
+                .ReturnsAsync(orders);
+
+            // Act
+            var result = await _customerService.GetAll();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(expectedCount, result.Count());
+        }
+        #endregion
+
+        #region Get
+        [Fact]
+        public async Task Get_Success_Test()
+        {
+            // Arrange
+            Order order = new Order { Id = 1 };
+
+            _mock
+                .Setup(x => x.Get(It.IsAny<int>()))
+                .ReturnsAsync(order);
+
+            // Act
+            var result = await _customerService.Get(It.IsAny<int>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(order.Id, result.Id);
+        }
+        #endregion
+
+        #region Create
+        [Fact]
+        public async Task Create_Success_Test()
+        {
+            // Arrange
+            Order newOrder = new Order { Id = 1 };
+
+            _mock
+                .Setup(x => x.Create(It.IsAny<Order>()))
+                .ReturnsAsync(newOrder);
+
+            // Act
+            var result = await _customerService.Create(It.IsAny<Order>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(newOrder.Id, result.Id);
+        }
+        #endregion
+
+        #region Update
+        [Fact]
+        public async Task Update_Success_Test()
+        {
+            // Arrange
+            Order order = new Order { Id = 1 };
+
+            _mock
+                .Setup(x => x.Create(It.IsAny<Order>()))
+                .ReturnsAsync(order);
+
+            // Act
+            var result = await _customerService.Update(It.IsAny<int>(), It.IsAny<Order>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(order.Id, result.Id);
+        }
+        #endregion
+
+        #region Delete
+        [Fact]
+        public async Task Delete_Success_Test()
+        {
+            // Arrange
+            Order order = new Order { Id = 1 };
+
+            _mock
+                .Setup(x => x.Delete(It.IsAny<int>()))
+                .ReturnsAsync(order);
+
+            // Act
+            var result = await _customerService.Delete(It.IsAny<int>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(order.Id, result.Id);
+        }
+        #endregion
     }
 }
