ed1dc5f6337319dad2a04459d3222d4a03f56ebe
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-09 08:33:28 +0200

    Bababooyeh

---
+++ TeamWebshopProject.Test/CSRD/Controllers/BasketControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using Moq;
+
+using TeamWebshopProject.API.CSRD.Controllers;
+using TeamWebshopProject.API.CSRD.Repository.Interfaces;
+using TeamWebshopProject.API.CSRD.Services.Interfaces;
+
+namespace TeamWebshopProject.Test.CSRD.Controllers
+{
+    public class BasketControllerTest
+    {
+        private readonly BasketController _controller;
+        private readonly Mock<IBasketService> basketService = new();
+
+        public BasketControllerTest()
+        {
+            _controller = new BasketController(basketService.Object);
+        }
+
+        []
+    }
+}
