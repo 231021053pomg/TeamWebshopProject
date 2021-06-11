538205e9bad1935f66c3c498b66cc588ad1de090
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-11 09:12:59 +0200

    NavBar menu added

--- TeamWebshopProject.Client/Client/src/app/app.module.ts
+++ TeamWebshopProject.Client/Client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FrontPageComponent } from './front-page/front-page.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { ProductpageComponent } from './productpage/productpage.component';
 import { CategorypageComponent } from './categorypage/categorypage.component';
@@ -12,7 +11,6 @@ import { AdminpageComponent } from './adminpage/adminpage.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FrontPageComponent,
     FrontpageComponent,
     ProductpageComponent,
     CategorypageComponent,
