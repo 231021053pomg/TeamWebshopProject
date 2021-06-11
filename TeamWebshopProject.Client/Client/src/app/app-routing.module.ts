3cf110413c512b8267112d20e95917f886e5d4b0
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-11 10:16:40 +0200

    App-routing set up and running

--- TeamWebshopProject.Client/Client/src/app/app-routing.module.ts
+++ TeamWebshopProject.Client/Client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { ProductpageComponent } from './productpage/productpage.component';
+import { CategorypageComponent } from './categorypage/categorypage.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'front-componennt', component: FrontpageComponent },
+  { path: 'product-component', component: ProductpageComponent },
+  { path: 'category-component', component: CategorypageComponent },
+  { path: 'admin-component', component: AdminpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
