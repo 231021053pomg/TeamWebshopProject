ddf018dbec56e5e27cf85bf4c06dd6de7c30938a
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-14 11:42:07 +0200

    yoink

--- TeamWebshopProject.Client/Client/src/app/app.module.ts
+++ TeamWebshopProject.Client/Client/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { FrontpageComponent } from './frontpage/frontpage.component';
 import { ProductpageComponent } from './productpage/productpage.component';
 import { CategorypageComponent } from './categorypage/categorypage.component';
 import { AdminpageComponent } from './adminpage/adminpage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { AdminpageComponent } from './adminpage/adminpage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TeamWebshopProject.Client/Client/src/app/categorypage/categorypage.component.ts
+++ TeamWebshopProject.Client/Client/src/app/categorypage/categorypage.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorypageComponent implements OnInit {
 
+  typeOfCategories: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+
   constructor() { }
 
   ngOnInit(): void {
