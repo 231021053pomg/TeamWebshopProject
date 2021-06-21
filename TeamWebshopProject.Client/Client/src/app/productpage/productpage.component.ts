088e3fbcf7f7ed3964877ecb49281e552e6312f3
BoringGuy <83692411+BoringGuy@users.noreply.github.com>
2021-06-21 10:13:51 +0200

    newitem page typescript implemented, just needs a few ajustements.

--- TeamWebshopProject.Client/Client/src/app/productpage/productpage.component.ts
+++ TeamWebshopProject.Client/Client/src/app/productpage/productpage.component.ts
@@ -13,6 +13,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductpageComponent implements OnInit {
   apiUrl: string = 'https://localhost:44365/apiItem';
+  id: number = 0;
 
   
   httpOptions = {
@@ -25,7 +26,7 @@ export class ProductpageComponent implements OnInit {
 
    }
 
-    id: number = 0;
+ 
     item: Item = {itemType: "", price: 0, discount: 0, discription: "", image: "" };
   
   
@@ -46,22 +47,4 @@ export class ProductpageComponent implements OnInit {
     
   }
 
-    /**
-    * Handle Http operation that failed.
-    * Let the app continue.
-    * @param operation - name of the operation that failed
-    * @param result - optional value to return as the observable result
-    */
-     private handleError<T>(operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
-         // TODO: send the error to remote logging infrastructure
-         console.error(error); // log to console instead
-  
-         // TODO: better job of transforming error for user consumption
-         console.log(`${operation} failed: ${error.message}`);
-  
-         // Let the app keep running by returning an empty result.
-         return of(result as T);
-      };
-    }
 }
