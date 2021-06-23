d994e1b57ea8231f352444f7db1f8c39937124cb
SyrupSniffer <69295837+SyrupSniffer@users.noreply.github.com>
2021-06-23 08:18:52 +0200

    Imports

--- TeamWebshopProject.Client/Client/src/app/app.module.ts
+++ TeamWebshopProject.Client/Client/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { ProductpageComponent } from './productpage/productpage.component';
@@ -10,19 +10,16 @@ import { CategorypageComponent } from './categorypage/categorypage.component';
 import { AdminpageComponent } from './adminpage/adminpage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NewitemComponent } from './newitem/newitem.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { HomeComponent } from './home/home.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReqisterComponent } from './reqister/reqister.component';
 
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './_LoginComps/login/login.component';
-import { ReqisterComponent } from './_LoginComps/reqister/reqister.component';
-import { ProfileComponent } from './_UserComps/profile/profile.component';
-import { BoardAdminComponent } from './_UserComps/board-admin/board-admin.component';
-import { BoardModeratorComponent } from './_UserComps/board-moderator/board-moderator.component';
-import { BoardUserComponent } from './_UserComps/board-user/board-user.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -32,15 +29,12 @@ import { HomeComponent } from './home/home.component';
     CategorypageComponent,
     AdminpageComponent,
     NewitemComponent,
-    LoginFormComponent,
-    RegisterComponent,
-    LoginComponent,
-    ReqisterComponent,
-    ProfileComponent,
+    HomeComponent,
     BoardAdminComponent,
     BoardModeratorComponent,
     BoardUserComponent,
-    HomeComponent
+    ProfileComponent,
+    ReqisterComponent
   ],
   imports: [
     BrowserModule,
